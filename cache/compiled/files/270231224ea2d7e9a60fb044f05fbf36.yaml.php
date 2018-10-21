1ea88705aea6c5f1b3baa1c6b1e7b8866060ade0
Emily Kolar <emkolardev@gmail.com>
2018-10-20 23:40:36 -0500

    clear cache

--- cache/compiled/files/270231224ea2d7e9a60fb044f05fbf36.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/emily/doxx/user/config/security.yaml',
-    'modified' => 1539822882,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/64894c7087eeea9a57612c8cb50d8fee.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/emily/doxx/user/config/plugins/highlight.yaml',
-    'modified' => 1539822882,
-    'data' => [
-        'theme' => 'learn'
-    ]
-];
